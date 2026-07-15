c0537d71c6955ff8b7677199e9d85155b89ab3b4
AndrewZAP1977 <31244624+AndrewZAP1977@users.noreply.github.com>
2026-07-15 10:56:37 +0300

    chore(dynamic-yield): load dual refinement last

--- scripts/DynamicYieldExperimentSafety.lua
+++ scripts/DynamicYieldExperimentSafety.lua
@@ -102,3 +102,4 @@ ADDynamicYield.MANEUVER_TRIGGER_WORLD_DISTANCE = 0
 source(Utils.getFilename("scripts/DynamicYieldExperimentSideConvention.lua", g_currentModDirectory))
 source(Utils.getFilename("scripts/DynamicYieldDualPass.lua", g_currentModDirectory))
 source(Utils.getFilename("scripts/DynamicYieldDualPassTiming.lua", g_currentModDirectory))
+source(Utils.getFilename("scripts/DynamicYieldDualPassRefinement.lua", g_currentModDirectory))
